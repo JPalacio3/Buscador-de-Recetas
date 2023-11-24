ea0255779152ee666172ed726787c1e32b1df3ae
Master <joelpalacio630@gmail.com>
2023-11-24 15:24:22 -0600

    feat: Añadiendo los detalles de ingredientes y Cantidades en la ventana Modal

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ function iniciarApp() {
     const selectCategorias = document.querySelector( '#categorias' );
     selectCategorias.addEventListener( 'change', seleccionarCategoria );
     const resultado = document.querySelector( '#resultado' );
+    const modal = new bootstrap.Modal( '#modal', {} );
 
     obtenerCategorias();
 
@@ -36,8 +37,16 @@ function iniciarApp() {
 
     // Función que se encarga de mostrar las recetas de la categoría seleccionada en el HTML
     function mostrarRecetas( recetas = [] ) {
+        limpiarHTML( resultado );
+
+        const heading = document.createElement( 'H2' );
+        heading.classList.add( 'text-center', 'text-black', 'my-5' );
+        heading.textContent = recetas.length ? 'Resultados' : 'No hay Resultados';
+        resultado.appendChild( heading );
+
         // Iterar en los resultados
         recetas.forEach( receta => {
+
             const { idMeal, strMeal, strMealThumb } = receta;
 
             const recetaContenedor = document.createElement( 'DIV' );
@@ -61,6 +70,11 @@ function iniciarApp() {
             const recetaButton = document.createElement( 'BUTTON' );
             recetaButton.classList.add( 'btn', 'btn-danger', 'w-100' );
             recetaButton.textContent = 'Ver Receta';
+            // recetaButton.dataset.bsTarget = "#modal";
+            // recetaButton.dataset.bsToggle = "modal";
+            recetaButton.onclick = function () {
+                seleccionarReceta( idMeal );
+            }
 
             // Inyectar en el HTML
             recetaCardBody.appendChild( recetaHeading );
@@ -70,7 +84,58 @@ function iniciarApp() {
             recetaContenedor.appendChild( recetaCard );
             resultado.appendChild( recetaContenedor );
         } )
+    }
 
+    function seleccionarReceta( id ) {
+        const url = `https://themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+        fetch( url )
+            .then( respuesta => respuesta.json() )
+            .then( resultado => mostrarRecetaModal( resultado.meals[ 0 ] ) );
+    }
+    function mostrarRecetaModal( receta ) {
+        const { idMeal, strInstructions, strMeal, strMealThumb } = receta;
+
+        // Añadir contenido al modal
+        const modalTitle = document.querySelector( '.modal .modal-title' );
+        const modalBody = document.querySelector( '.modal .modal-body' );
+
+        modalTitle.textContent = strMeal;
+        modalBody.innerHTML = `
+            <img class="img-fluid" src="${strMealThumb}" alt="receta: ${strMeal}" />
+            <h3 class="my-3">Instrucciones</h3>
+            <p>${strInstructions}</p>
+            <h3>Ingredientes y Cantidades</h3>
+        `;
+
+        const listGroup = document.createElement( 'UL' );
+        listGroup.classList.add( 'list-group' );
+        // Mostrar cantidades e ingredientes
+        for ( let i = 1; i <= 20; i++ ) {
+            if ( receta[ `strIngredient${i}` ] ) {
+                const ingrediente = receta[ `strIngredient${i}` ];
+                const cantidad = receta[ `strMeasure${i}` ];
+
+                // Listar los ingredientes en el HTML
+                const ingredienteLi = document.createElement( 'LI' );
+                ingredienteLi.classList.add( 'list-group-item' );
+                ingredienteLi.textContent = `${ingrediente}   -   ${cantidad}`;
+
+                listGroup.appendChild( ingredienteLi );
+
+                console.log( `ingrediente: ${ingrediente} - ${cantidad}` )
+            }
+        }
+
+        modalBody.appendChild( listGroup );
+
+        // Muestra el modal
+        modal.show();
+    }
+
+    function limpiarHTML( selector ) {
+        while ( selector.firstChild ) {
+            selector.removeChild( selector.firstChild );
+        }
     }
 }
 
