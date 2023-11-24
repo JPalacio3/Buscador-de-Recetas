daf79c016da665bfc7f9aa06a5118038dc51d7b5
Master <joelpalacio630@gmail.com>
2023-11-23 21:52:04 -0600

    feat: Conexión a la API de recetas y listar las recetas de las categorías en el HTML

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ function iniciarApp() {
 
     const selectCategorias = document.querySelector( '#categorias' );
     selectCategorias.addEventListener( 'change', seleccionarCategoria );
+    const resultado = document.querySelector( '#resultado' );
 
     obtenerCategorias();
 
@@ -27,8 +28,50 @@ function iniciarApp() {
     // Función que se encarga de mostrar las categorías en el HTML
     function seleccionarCategoria( e ) {
         const categoria = e.target.value;
+        const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`;
+        fetch( url )
+            .then( respuesta => respuesta.json() )
+            .then( resultado => mostrarRecetas( resultado.meals ) )
     }
 
+    // Función que se encarga de mostrar las recetas de la categoría seleccionada en el HTML
+    function mostrarRecetas( recetas = [] ) {
+        // Iterar en los resultados
+        recetas.forEach( receta => {
+            const { idMeal, strMeal, strMealThumb } = receta;
+
+            const recetaContenedor = document.createElement( 'DIV' );
+            recetaContenedor.classList.add( 'col-md-4' );
+
+            const recetaCard = document.createElement( 'DIV' );
+            recetaCard.classList.add( 'card', 'mb-4' );
+
+            const recetaImagen = document.createElement( 'IMG' );
+            recetaImagen.classList.add( 'card-img-top' );
+            recetaImagen.alt = `imagen de la receta ${strMeal}`;
+            recetaImagen.src = strMealThumb;
+
+            const recetaCardBody = document.createElement( 'DIV' );
+            recetaCardBody.classList.add( 'card-body' );
+
+            const recetaHeading = document.createElement( 'H3' );
+            recetaHeading.classList.add( 'card-title', 'mb-3' );
+            recetaHeading.textContent = strMeal;
+
+            const recetaButton = document.createElement( 'BUTTON' );
+            recetaButton.classList.add( 'btn', 'btn-danger', 'w-100' );
+            recetaButton.textContent = 'Ver Receta';
+
+            // Inyectar en el HTML
+            recetaCardBody.appendChild( recetaHeading );
+            recetaCardBody.appendChild( recetaButton );
+            recetaCard.appendChild( recetaImagen );
+            recetaCard.appendChild( recetaCardBody );
+            recetaContenedor.appendChild( recetaCard );
+            resultado.appendChild( recetaContenedor );
+        } )
+
+    }
 }
 
 document.addEventListener( 'DOMContentLoaded', iniciarApp );
